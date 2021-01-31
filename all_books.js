91ef079cd6abec21976559da77aedba94ae735f6
Priyanshu <priyanshushukla9801@gmail.com>
2021-01-31 17:35:55 +0530

    Footer changes and API integration

---
+++ all_books.js
@@ -0,0 +1,148 @@
+const params = new URLSearchParams(window.location.search)
+let id_of_publisher
+for (const param of params) {
+    if (param != 'id') {
+        id_of_publisher = parseInt(param)
+    }
+}
+
+let whole_books_arr = []
+let current_page_number = 1
+let total_books = 0
+
+let formdata = new FormData();
+formdata.append("search", id_of_publisher);
+
+let requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow'
+};
+
+fetch("http://159.89.170.179/api/book/filter/fs/publisher/", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        whole_books_arr = result.data
+        total_books = whole_books_arr.length
+        console.log(whole_books_arr)
+        console.log(total_books)
+        paginate()
+    })
+    .catch(error => console.log('error', error));
+
+document.getElementsByClassName('books_flexbox')[0].innerHTML = ''
+document.getElementsByClassName('books_flexbox')[1].innerHTML = ''
+document.getElementsByClassName('next')[0].style.display = 'flex'
+
+if (total_books < 9) {
+    document.getElementsByClassName('next')[0].style.display = 'none'
+    console.log('hi')
+}
+
+function paginate() {
+    document.getElementsByClassName('books_flexbox')[0].innerHTML = ''
+    document.getElementsByClassName('books_flexbox')[1].innerHTML = ''
+    for (var i = (8 * (current_page_number - 1)); i < 8 * current_page_number; i++) {
+        if (i == total_books - 1) {
+            if (i - (8 * (current_page_number - 1)) < 4) {
+
+                populate_books(whole_books_arr[i], 0)
+            }
+            else {
+                populate_books(whole_books_arr[i], 1)
+            }
+            break
+        }
+        if (i - (8 * (current_page_number - 1)) < 4) {
+
+            populate_books(whole_books_arr[i], 0)
+        }
+        else {
+            populate_books(whole_books_arr[i], 1)
+        }
+    }
+}
+
+function populate_books(obj, row) {
+    var container_div = document.createElement('div')
+    container_div.classList.add('book_card')
+    var onclick = document.createAttribute('onclick')
+    onclick.value = `location.href='book.html?id=${obj.id}'`
+    container_div.setAttributeNode(onclick)
+
+    var book_img_container = document.createElement('div')
+    book_img_container.classList.add('book_img_container')
+
+    var img = document.createElement('img')
+    var src = document.createAttribute('src')
+    src.value = obj.thumbnail
+    img.setAttributeNode(src)
+    var class_name = document.createAttribute('class')
+    class_name.value = 'book_img'
+    img.setAttributeNode(class_name)
+
+    book_img_container.appendChild(img)
+
+    var book_text = document.createElement('div')
+    book_text.classList.add('book_text')
+
+    var book_title = document.createElement('div')
+    book_title.classList.add('book_title')
+    book_title.innerHTML = obj.title
+
+    var book_author = document.createElement('div')
+    book_author.classList.add('book_author')
+    book_author.innerHTML = obj.author
+
+    var book_price_container = document.createElement('div')
+    book_price_container.classList.add('book_price_container')
+
+    var new_price = document.createElement('div')
+    new_price.classList.add('new_price')
+    new_price.innerHTML = '&#8377' + obj.expected_price
+
+    var old_price = document.createElement('div')
+    old_price.classList.add('old_price')
+    old_price.innerHTML = '&#8377' + obj.price_indian_currency
+
+    var discount = document.createElement('div')
+    discount.classList.add('discount')
+    discount.innerHTML = 'Save ' + obj.discount + '%'
+
+    book_price_container.appendChild(new_price)
+    book_price_container.appendChild(old_price)
+    book_price_container.appendChild(discount)
+
+    book_text.appendChild(book_title)
+    book_text.appendChild(book_author)
+    book_text.appendChild(book_price_container)
+
+    container_div.appendChild(book_img_container)
+    container_div.appendChild(book_text)
+
+    document.getElementsByClassName('books_flexbox')[row].appendChild(container_div)
+}
+
+function next() {
+    current_page_number++
+    if (current_page_number == 2) {
+        document.getElementsByClassName('prev')[0].style.display = 'flex'
+
+    }
+    if (Math.ceil(total_books / 8) == current_page_number) {
+        document.getElementsByClassName('next')[0].style.display = 'none'
+    }
+    paginate()
+}
+
+function prev() {
+    current_page_number--
+    if (current_page_number == Math.ceil(total_books / 8) - 1) {
+        document.getElementsByClassName('next')[0].style.display = 'flex'
+    }
+    if (current_page_number == 1) {
+        document.getElementsByClassName('prev')[0].style.display = 'none'
+
+    }
+    paginate()
+}
---
+++ index.js
@@ -0,0 +1,71 @@
+let publishers_arr = []
+
+
+fetch("http://159.89.170.179/api/publisher/list/")
+    .then(response => response.json())
+    .then(data => {
+        publishers_arr = data.data
+        populate_publilshers()
+    })
+    .catch(error => console.log('error', error));
+
+
+function populate_publilshers() {
+    var length = publishers_arr.length
+    console.log(length, publishers_arr)
+    var number_of_rows = Math.ceil(length / 4)
+    var current = 0
+    document.getElementsByClassName('publishers_grid_container')[0].innerHTML = ''
+
+    for (var x = 0; x < number_of_rows; x++) {
+        var container_div = document.createElement('div')
+        container_div.classList.add('publishers_flexbox')
+        document.getElementsByClassName('publishers_grid_container')[0].appendChild(container_div)
+
+        if (length < 4) {
+            console.log(length)
+            for (var i = 0; i < length; i++) {
+                create_stalls(publishers_arr[current], x)
+                current++
+            }
+            break
+        }
+        else {
+            for (var i = 0; i < 4; i++) {
+                create_stalls(publishers_arr[current], x)
+                current++
+            }
+            length = length - 4
+        }
+
+    }
+}
+
+function create_stalls(obj, x) {
+    var div = document.createElement('div')
+    div.classList.add('stalls')
+    var image_div = document.createElement('div')
+    var img = document.createElement('img')
+    var src = document.createAttribute('src')
+    var img_class = document.createAttribute('class')
+    var onclick = document.createAttribute('onclick')
+    var root_url = 'http://159.89.170.179/media/'
+    src.value = root_url + obj.logo
+    img_class.value = 'stalls_image'
+    onclick.value = `location.href='all_books.html?id=${obj.id}'`
+
+    img.setAttributeNode(src)
+    img.setAttributeNode(img_class)
+    img.setAttributeNode(onclick)
+    image_div.appendChild(img)
+    div.appendChild(image_div)
+
+    var text_div = document.createElement('div')
+    text_div.classList.add('stalls_heading')
+    text_div.innerHTML = obj.name
+    div.appendChild(text_div)
+
+    document.getElementsByClassName('publishers_flexbox')[x].appendChild(div)
+
+
+}
